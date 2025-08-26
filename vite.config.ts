754d22064e14ecb7e2fa6a06ecaf78deded18cb2
FahrulRozzy <fahrulrozzy547@gmail.com>
2025-08-26 09:17:50 +0700

    chore: add GitHub Pages deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,5 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
+	base: '/cy-block/', // penting untuk GitHub Pages
 });
