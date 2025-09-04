0b12dcaf6ea838bd0f3d23e05d8895a3bb1759f5
JustinDjafar <kristaldijustin@gmail.com>
2025-09-04 16:03:44 +0700

    Add authenticated marunda.tsx

---
+++ src/routes/authenticated/depo-marunda.tsx
@@ -0,0 +1,5 @@
+import DisplayYard from "../../components/display-yard";
+
+export default function DepoMarunda() {
+    return <DisplayYard name="MARUNDA" containerSize={{ size20: [1, 2.6, 1], size40: [15.3, 2.6, 2.64] }} />;
+}
